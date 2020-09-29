31c62ac754efcc5c05b4af1c807d2de5ea1c28c2
CobuEric <71341844+CobuEric@users.noreply.github.com>
2020-09-29 15:16:50 -0700

    Add files via upload

---
+++ Eric_python_class01.py
@@ -0,0 +1,22 @@
+print(2**64)
+
+total_seconds= (2**64)
+
+total_minutes= total_seconds//60
+print(total_minutes)
+
+total_hours = total_minutes//60
+print(total_hours)
+
+total_days= total_hours//24
+print(total_days)
+
+total_years= total_days//365
+print(total_years)
+
+total_gen= total_years//100
+print(total_gen)
+#And that is how many generations will be working on a puzzle
+
+name=input("What is your name.?")
+print("Hello",name)
